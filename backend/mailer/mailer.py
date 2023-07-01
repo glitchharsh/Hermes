8e527d4504b5ba4435739e04103a42e82ba385d2
Harsh <prajapatiharsh.210@gmail.com>
2023-07-01 23:10:43 +0530

    separated AppEmail and Email classes

--- backend/mailer/mailer.py
+++ backend/mailer/mailer.py
@@ -1,8 +1,28 @@
 import smtplib
-from mailer.templates import otp_template
+from mailer.templates import Templates
 
 
 class Email:
+    def __init__(self,sender,reciever,password,name,msg):
+        self.sender = sender
+        self.reciever = reciever
+        self.password = password
+        self.name = name
+        self.msg = msg
+    
+    def send_message(self):
+        message = self.msg.format(
+            receiver=self.receiver, name=self.name)
+        try:
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+                smtp.login(self.sender, self.password)
+                smtp.sendmail(self.sender, self.receiver, message)
+            return True
+        except Exception as ex:
+            print(ex)
+            return False
+
+class AppEmail(Email):
 
     def __init__(self, receiver, name=""):
         self.sender = "noreply.adhyaay@gmail.com"
