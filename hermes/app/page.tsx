f268db558209a03806827f5d82e58f059bebb00f
Raunak173 <raunakagarwal173@gmail.com>
2023-07-01 18:00:16 +0530

    boilerplate added

---
+++ hermes/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-red-300">
+      <div>
+        <h1 className="font-bold text-center text-[50px]">Welcome to HERMES</h1>
+        <p className="text-center text-4xl text-blue-700 italic">
+          (SMS Generator)
+        </p>
+      </div>
+    </div>
+  );
+}
